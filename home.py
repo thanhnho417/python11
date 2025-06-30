f792bd56f4c9119099aaf733685cba5776754756
thanhnho417 <trieudoquoc417@outlook.com>
2025-06-30 09:00:08 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,18 +4,13 @@ import tempfile
 import uuid
 from PIL import Image
 from imgconverter import ImageConverter
-
+import zipfile
+import io
 # Cấu hình trang
 st.set_page_config(
     page_icon='🔄',
     page_title='File Converter - Designed by oce',
     layout="wide", initial_sidebar_state="auto"
-)
-st.markdown(
-    """
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    """,
-    unsafe_allow_html=True
 )
 st.title('File Converter - Công cụ chuyển đổi định dạng File')
 st.subheader('Chọn chức năng')
@@ -23,37 +18,65 @@ tab1, tab2, tab3, tab4 = st.tabs(['Hình ảnh', 'Tài liệu', 'Âm thanh', 'Vi
 
 with tab1:
     st.header('Chuyển đổi hình ảnh')
-    file_uploaded = st.file_uploader('Tải ảnh lên', type=ImageConverter.get_file_format_supported())
+    files_uploaded = st.file_uploader('Tải ảnh lên', type=ImageConverter.get_file_format_supported(), accept_multiple_files=True)
 
-    if file_uploaded:
-        st.image(file_uploaded, caption='Ảnh muốn chuyển đổi', width=200)
-        output = st.selectbox('Chọn định dạng đầu ra:', ImageConverter.get_file_format_supported())
-
-        if st.button('Chuyển đổi ảnh'):
+    if files_uploaded:
+        cols = st.columns(len(files_uploaded))
+        for col, file in zip(cols,files_uploaded):
+            col.image(file, width=200)
+            
+            
+            
+        output = st.selectbox('Chọn định dạng đầu ra cho tất cả ảnh:', ImageConverter.get_file_format_supported())
+        if st.button('Chuyển đổi'):
+            converted_file = []
             with st.spinner('Đang xử lí...'):
-                file_ext = file_uploaded.name.split('.')[-1]
-                temp_path = os.path.join(tempfile.gettempdir(), f"oceconvert_{uuid.uuid4()}.{file_ext}")
-
-                with open(temp_path, 'wb') as f:
-                    f.write(file_uploaded.getbuffer())
-
-                success, result = ImageConverter.file_convert(temp_path, output)
-
-                if os.path.exists(temp_path):
-                    os.remove(temp_path)
-
-                if success and os.path.exists(result):
-                    st.success('Hoàn thành')
-                    mime_map = {'jpg': 'image/jpeg', 'jpeg': 'image/jpeg', 'png': 'image/png', 'webp': 'image/webp'}
-                    mime_type = mime_map.get(output.lower(), f'image/{output.lower()}')
-
-                    with open(result, 'rb') as f:
-                        st.download_button(label='Tải xuống', data=f, file_name=os.path.basename(result), mime=mime_type)
+                mine_map = {
+                    'jpg': 'image/jpeg', 'jpeg': 'image/jpeg',
+                    'png': 'image/png', 'webp': 'image/webp'
+                }
+                
+                for file in files_uploaded:
+                    file_ext = file.name.split('.')[-1]
+                    temp_input_path = os.path.join(tempfile.gettempdir(), f'oceconvert_{uuid.uuid4()}.{file_ext}')
                     
-                    os.remove(result)
-                else:
-                    st.error('Đã xảy ra lỗi, có thể file không hỗ trợ cho định dạng nay, vui lòng thử lại hoặc chuyển sang định dạng khác')
-
+                    with open(temp_input_path, 'wb') as f:
+                        f.write(file.getbuffer())
+                    success, result_path = ImageConverter.file_convert(temp_input_path, output)
+                    
+                    if os.path.exists(temp_input_path):
+                        os.remove(temp_input_path)
+                        
+                    if success and os.path.exists(result_path):
+                        
+                        mine_type = mine_map.get(output.lower(), f'image/{output.lower()}')
+                        converted_file.append((os.path.splitext(file.name)[0], result_path))
+                    else: st.error(f'Lỗi chuyển đổi file {file.name}')
+                    
+                if converted_file:
+                    zip = io.BytesIO()
+                    with zipfile.ZipFile(zip, 'w') as zipf:
+                        for base, file_path in converted_file:
+                            arcname = f'{base}.{output}'
+                            zipf.write(file_path, arcname=arcname)
+                            os.remove(file_path)
+                    
+                    zip.seek(0)
+                    st.download_button(label='Tải xuống toàn bộ', data=zip, file_name='ocefileconverter.zip', mime='application/zip')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 with tab2:
     st.header('Chuyển đổi tài liệu')
     st.info('Under construction')
